de3f89319c2f6aa5420c9ba3a8cf2841acf097f8
safa954 <100359594+safa954@users.noreply.github.com>
2022-09-21 17:51:54 +0300

    main

--- mechanic/admin/detele_mechanic.php
+++
@@ -1,8 +0,0 @@
-<?php require_once '../app/config/db.php';?>
-
-<?php
-
-$sql = "DELETE FROM mechanic where mechanic_id = '".$_GET['mechanic_id']."'";
-if (mysqli_query($link, $sql)) {
-    header("location: mechanic.php");
-}
